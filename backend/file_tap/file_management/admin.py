e0371b2fbd42144126c6ccd8bad5ba049936b924
Ashish Auti <ashishauti123@gmail.com>
2025-05-29 19:22:43 +0530

    feat : add upload file models and custome admin

--- backend/file_tap/file_management/admin.py
+++ backend/file_tap/file_management/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from base.base_admin import BaseAdmin
+
+from .models import UploadedFile
+
+
+@admin.register(UploadedFile)
+class UloadedFileAdmin(BaseAdmin):
+    fields = ("name", "file", "created_at", "updated_at")
+    list_display = ("name", "created_at")
--- backend/file_tap/file_management/models.py
+++ backend/file_tap/file_management/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+from base.base_models import BaseModel
+
+
+class UploadedFile(BaseModel):
+    name = models.CharField(
+        max_length=150,
+        help_text="Name of the uploaded file",
+        unique=True,
+        verbose_name=_("Uploaded File Name"),
+    )
+    file = models.FileField(
+        upload_to="text_files/",
+        verbose_name=_("Uploaded Text File"),
+        help_text="Text file",
+        null=True,
+        blank=True,
+    )
+
+    class Meta:
+        verbose_name = _("File")
+        verbose_name_plural = _("Files")
+
+    def __str__(self):
+        return self.name
